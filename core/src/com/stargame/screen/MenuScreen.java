576fc09ef2b608d185fd8e9ae7ea0224927ef1b5
Mariia <marisergiv@gmail.com>
2021-06-07 14:21:22 +0300

    lesson 4

--- core/src/com/stargame/screen/MenuScreen.java
+++ core/src/com/stargame/screen/MenuScreen.java
@@ -1,58 +1,76 @@
 package com.stargame.screen;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.stargame.base.BaseScreen;
 import com.stargame.math.Rect;
 import com.stargame.sprite.Background;
-import com.stargame.sprite.Logo;
+import com.stargame.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture bg;
-    private Texture plate;
+    private static final int STAR_COUNT = 256;
 
+    private Texture bg;
+    private TextureAtlas menuAtlas;
     private Background background;
-    private Logo logo;
+    private Star[] stars;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("space.png");
         background = new Background(bg);
-        plate = new Texture("plate.png");
-        logo = new Logo(plate);
+        menuAtlas = new TextureAtlas("menuAtlas.atlas");
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(menuAtlas);
+        }
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
-        logo.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        logo.update(delta);
-        ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
-        batch.begin();
-        background.draw(batch);
-        logo.draw(batch);
-        batch.end();
+        update(delta);
+        draw();
     }
 
     @Override
     public void dispose() {
         super.dispose();
         bg.dispose();
-        plate.dispose();
+        menuAtlas.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        logo.touchDown(touch,pointer,button);
         return false;
+    }
+
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
+    }
+
+    private void draw() {
+        ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
+        batch.begin();
+        background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        batch.end();
     }
 }
