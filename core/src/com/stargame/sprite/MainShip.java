eff2fe456ee62afbaab3866503b883b50c3efed6
Mariia <marisergiv@gmail.com>
2021-06-07 22:55:51 +0300

    lesson 5

--- core/src/com/stargame/sprite/MainShip.java
+++ core/src/com/stargame/sprite/MainShip.java
@@ -10,6 +10,7 @@ public class MainShip extends Sprite {
 
     private static final float HEIGHT = 0.15f;
     private static final float PADDING = 0.05f;
+    private static final int INVALID_POINTER = -1;
 
     private final Vector2 v0 = new Vector2(0.5f, 0);
     private final Vector2 v = new Vector2();
@@ -17,12 +18,18 @@ public class MainShip extends Sprite {
     private boolean pressedLeft;
     private boolean pressedRight;
 
+    private int leftPointer = INVALID_POINTER;
+    private int rightPointer = INVALID_POINTER;
+
+    private Rect worldBounds;
+
     public MainShip(TextureAtlas atlas) {
         super(atlas.findRegion("main_ship"), 1, 2, 2);
     }
 
     @Override
     public void resize(Rect worldBounds) {
+        this.worldBounds = worldBounds;
         setHeightProportion(HEIGHT);
         setBottom(worldBounds.getBottom() + PADDING);
     }
@@ -30,15 +37,57 @@ public class MainShip extends Sprite {
     @Override
     public void update(float delta) {
         pos.mulAdd(v, delta);
+//        if(getRight() > worldBounds.getRight()) {
+//            setRight(worldBounds.getRight());
+//            stop();
+//        }
+//        if(getLeft() < worldBounds.getLeft()) {
+//            setLeft(worldBounds.getLeft());
+//            stop();
+//        }
+        if(getLeft() > worldBounds.getRight()) {
+            setRight(worldBounds.getLeft());
+        }
+        if(getRight() < worldBounds.getLeft()) {
+            setLeft(worldBounds.getRight());
+        }
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        if (touch.x < worldBounds.pos.x) {
+            if (leftPointer != INVALID_POINTER) {
+                return false;
+            }
+            leftPointer = pointer;
+            moveLeft();
+        } else {
+            if (rightPointer != INVALID_POINTER) {
+                return false;
+            }
+            rightPointer = pointer;
+            moveRight();
+        }
         return false;
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
+        if (pointer == leftPointer) {
+            leftPointer = INVALID_POINTER;
+            if (rightPointer != INVALID_POINTER) {
+                moveRight();
+            } else {
+                stop();
+            }
+        } else if (pointer == rightPointer) {
+            rightPointer = INVALID_POINTER;
+            if (leftPointer != INVALID_POINTER) {
+                moveLeft();
+            } else {
+                stop();
+            }
+        }
         return false;
     }
 
@@ -63,7 +112,7 @@ public class MainShip extends Sprite {
             case Input.Keys.A:
             case Input.Keys.LEFT:
                 pressedLeft = false;
-                if(pressedRight) {
+                if (pressedRight) {
                     moveRight();
                 } else {
                     stop();
